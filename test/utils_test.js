9e4cdb6512f96fcb20f9182717b4fac0047ee5dc
Alex Koppel <alex@alexkoppel.com>
2013-07-27 11:18:34 -0500

    Get tests working

---
+++ test/utils_test.js
@@ -0,0 +1,19 @@
+var expect = require('expect.js');
+var utils = require("../lib/utils");
+
+describe("Utils", function() {
+  describe("dominantMovement", function() {
+    it("returns the dominant movement as a direction and distance", function() {
+      var movementOne = [1, 2, 3];
+      var movementTwo = [2, -4, 4];
+
+      expect(utils.dominantMovement(movementOne, movementTwo)).to.eql({direction: "y", distance: -6});
+    })
+
+    it("throws an exception if one or both values are null", function() {
+      expect(function() { utils.dominantMovement(null, [1, 2, 3]) }).to.throwException();
+      expect(function() { utils.dominantMovement([1, 2, 3], null) }).to.throwException();
+      expect(function() { utils.dominantMovement(null, null) }).to.throwException();
+    })
+  })
+})
